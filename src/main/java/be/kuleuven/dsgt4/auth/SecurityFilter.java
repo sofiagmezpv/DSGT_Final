a8c0a16c1523aa7317346d158b32f5f2816f5fdb
hofkensstijn <hofkens.stijn@gmail.com>
2024-04-26 11:25:30 +0200

    initial project

---
+++ src/main/java/be/kuleuven/dsgt4/auth/SecurityFilter.java
@@ -0,0 +1,101 @@
+package be.kuleuven.dsgt4.auth;
+
+import be.kuleuven.dsgt4.User;
+import com.fasterxml.jackson.core.io.JsonStringEncoder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+
+
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // TODO: (level 1) decode Identity Token and assign correct email and role
+        // TODO: (level 2) verify Identity Token
+        String token = request.getHeader("Authorization");
+        // TODO: navragen waarom FireBaseAUth niet werkt
+        //FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+        //String uid = decodedToken.getUid();
+
+        System.out.println(token);
+        var user = new User("test@example.com", "manager");
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new FirebaseAuthentication(user));
+
+
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        return !path.startsWith("/api");
+    }
+
+    private static class FirebaseAuthentication implements Authentication {
+        private final User user;
+
+        FirebaseAuthentication(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            if (user.isManager()) {
+                return List.of(new SimpleGrantedAuthority("manager"));
+            } else {
+                return new ArrayList<>();
+            }
+        }
+
+        @Override
+        public Object getCredentials() {
+            return null;
+        }
+
+        @Override
+        public Object getDetails() {
+            return null;
+        }
+
+        @Override
+        public User getPrincipal() {
+            return this.user;
+        }
+
+        @Override
+        public boolean isAuthenticated() {
+            return true;
+        }
+
+        @Override
+        public void setAuthenticated(boolean b) throws IllegalArgumentException {
+
+        }
+
+        @Override
+        public String getName() {
+            return null;
+        }
+    }
+}
+
