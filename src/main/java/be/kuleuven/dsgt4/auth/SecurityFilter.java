6afd4f4147dbb53e5f24b1430d67cd082b42bd33
Mathilde Blanc <maeblanc@gmail.com>
2024-05-03 12:07:45 +0200

    front end first version

--- src/main/java/be/kuleuven/dsgt4/auth/SecurityFilter.java
+++ src/main/java/be/kuleuven/dsgt4/auth/SecurityFilter.java
@@ -18,8 +18,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseToken;
+
 
 
 
